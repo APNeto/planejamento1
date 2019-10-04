5a58d09cfd1d539665a696c88118398371e68fd8
APNeto <penaneto@gmail.com>
2019-10-04 01:54:48 -0300

    Corrigido troca de i e j

--- main.cpp
+++ main.cpp
@@ -5,18 +5,28 @@
 #include <queue>
 #include <set>
 #include <cmath>
+#include <chrono>
+#include <algorithm>
 
-std::array<int,9> sol = {0,1,2,3,4,5,6,7,8,9};
+using tipo = std::array<char, 9>;
+
+tipo sol = {0,1,2,3,4,5,6,7,8};
 //std::vector<short> passos;
+
+class board;
+
+std::priority_queue<board> open;
+std::set<tipo> closed;
+
 
 class board {
 public:
-    std::array<int,9> ordem;
+    tipo ordem;
     int valor_g;
     int valor_h;
     int valor_f;
     
-    board(std::array<int,9> board_in, g=0): ordem(board_in), valor_g(g)
+    board(tipo board_in, int g=0): ordem(board_in), valor_g(g)
     {
         valor_h = calcManhattan();
         updateF();
@@ -46,116 +56,115 @@ public:
         int num = 0;
         
         for(int i = 0; i < 3 ; i++)
-            for(int j = 0; j , 3; j++)
+            for(int j = 0; j < 3; j++)
             {
                 num = this->ordem.at(3*i + 1*j);
                 dist += std::abs(num % 3 - i) + std::abs(num / 3 - j);
             }
         return dist;
     }
+
     
-    void expande(std::priority_queue<board> &open){
-        int valor_child_g = valor_g + 1;
-        int pos_0 = 0;
-        int i,j; // para verificar em qual linha e em qual coluna está o 0
-        
-        {
-            std::array<int>::iterator;
-            it = std::find (ordem.begin(), ordem.end(), 0); 
-            if (it != ordem.end()) 
-            { 
-                //std::cout << "Element " << 0 <<" found at position : " ; 
-                pos_0 = it - ordem.begin(); 
-            }
-        }
-        
-        i = pos_0 % 3; // linha atual do 0
-        j = pos_0 / 3; // coluna atual do 0
-        
-        // pos_0 += 1; // move direita
-        // pos_0 -= 1; // move esquerda
-        // pos_0 -= 3; // move cima
-        // pos_0 += 3; // move baixo
-        
-        if(i != 0) // 0 não está na linha mais de cima
-        {
-            array<int, 9> novo_board(this->ordem);
-            novo_board.at(pos_0) = novo_board.at(pos_0 - 3);
-            novo_board.at(pos_0 - 3) = 0; //move cima
-            
-            //confere_se_em_closed
-            if (auto search = closed.find(novo_board); search != example.end()) {
-                //std::cout << "Found " << (*search) << '\n';
-            } else {
-                //std::cout << "Not found\n";
-                auto* new_board_ptr = new board(novo_board, child_valor_g);
-                open.push(*new_board_ptr);
-            }
-        }
-        if(i != 2) expande_move_baixo(){
-            array<int, 9> novo_board(this->ordem);
-            novo_board.at(pos_0) = novo_board.at(pos_0 + 3);
-            novo_board.at(pos_0 + 3) = 0; //move cima
-            
-            //confere_se_em_closed
-            if (auto search = closed.find(novo_board); search != example.end()) {
-                //std::cout << "Found " << (*search) << '\n';
-            } else {
-                //std::cout << "Not found\n";
-                auto* new_board_ptr = new board(novo_board, child_valor_g);
-                open.push(*new_board_ptr);
-            }
-        }
-        if(j != 0) expande_move_esquerda(); // 0 não está na coluna mais da esquerda
-        {
-            array<int, 9> novo_board(this->ordem);
-            novo_board.at(pos_0) = novo_board.at(pos_0 - 1);
-            novo_board.at(pos_0 - 1) = 0; //move cima
-            
-            //confere_se_em_closed
-            if (auto search = closed.find(novo_board); search != example.end()) {
-                //std::cout << "Found " << (*search) << '\n';
-            } else {
-                //std::cout << "Not found\n";
-                auto* new_board_ptr = new board(novo_board, child_valor_g);
-                open.push(*new_board_ptr);
-            }
-        }
-        if(j != 2) expande_move_direita();
-        {
-            array<int, 9> novo_board(this->ordem);
-            novo_board.at(pos_0) = novo_board.at(pos_0 - 1);
-            novo_board.at(pos_0 - 1) = 0; //move cima
-            
-            //confere_se_em_closed
-            if (auto search = closed.find(novo_board); search != example.end()) {
-                //std::cout << "Found " << (*search) << '\n';
-            } else {
-                //std::cout << "Not found\n";
-                auto* new_board_ptr = new board(novo_board, child_valor_g);
-                open.push(*new_board_ptr);
-            }
-        }
-    }
-}
+};
 
-
-std::priority_queue<board> open;
-std::set<array<int,9>> closed;
 
 bool isObjetivo(board &tentativa)
 {
-    for(int i=0; i<9; i++) if( tentativa->ordem.at(i) != sol.at(i) ) return false;
+    for(char i=0; i<9; i++) if( tentativa.ordem.at(i) != sol.at(i) ) return false;
     return true;
 }
 
-void calc(board &inicial)
+void in_closed_or_open(tipo novo_board, int valor_g){
+    //std::cout << "Em in_closed_or_open\n";
+    int child_valor_g = valor_g + 1;
+
+    //confere_se_em_closed
+    if (auto search = closed.find(novo_board); search != closed.end()) {
+        std::cout << "Found in closed" << '\n';
+
+    } else {
+        //std::cout << "Not found\n";
+        auto* new_board_ptr = new board(novo_board, child_valor_g);
+        //std::cout << "Not found Error\n";
+        open.push(*new_board_ptr);
+    }
+}
+    
+void expande(board &pai){
+
+    int pos_0 = 0;
+    int i,j; // para verificar em qual linha e em qual coluna está o 0
+    
+    {
+        tipo::iterator it;
+        it = std::find (pai.ordem.begin(), pai.ordem.end(), 0); 
+        if (it != pai.ordem.end()) 
+        { 
+            //std::cout << "Element " << 0 <<" found at position : \n" ; 
+            pos_0 = it - pai.ordem.begin(); 
+        }
+        else std::cout << "Ha um erro de acesso a array na linha\n";
+    }
+    
+    i = pos_0 / 3; // linha atual do 0
+    j = pos_0 % 3; // coluna atual do 0
+
+    std::cout << "0 ta na linha " << i << " e coluna " << j << " \n";
+    
+    if(i != 0) // 0 não está na linha mais de cima
+    {
+        tipo novo_board = pai.ordem;
+        novo_board.at(pos_0) = novo_board.at(pos_0 - 3);
+        novo_board.at(pos_0 - 3) = 0; //move cima
+        
+        in_closed_or_open(novo_board, pai.valor_g);
+        
+    }
+    if(i != 2) // 0 não está na linha mais de baixo
+    { 
+        tipo novo_board = pai.ordem;
+        novo_board.at(pos_0) = novo_board.at(pos_0 + 3);
+        novo_board.at(pos_0 + 3) = 0; //move cima
+            
+        in_closed_or_open(novo_board, pai.valor_g);
+    }
+    if(j != 0) // 0 não está na coluna mais da esquerda
+    {
+        tipo novo_board = pai.ordem;
+        novo_board.at(pos_0) = novo_board.at(pos_0 - 1);
+        novo_board.at(pos_0 - 1) = 0; //move cima
+        
+        in_closed_or_open(novo_board, pai.valor_g);
+    }
+    if(j != 2) // 0 não está na coluna mais da direita
+    {
+        tipo novo_board = pai.ordem;
+        novo_board.at(pos_0) = novo_board.at(pos_0 + 1);
+        novo_board.at(pos_0 + 1) = 0; //move cima
+        
+        in_closed_or_open(novo_board, pai.valor_g);
+        //confere_se_em_closed
+        // if (auto search = closed.find(novo_board); search != closed.end()) {
+        //     std::cout << "Found " << '\n';
+        // } else {
+        //     std::cout << "Not found\n";
+        //     auto *new_board_ptr = new board(novo_board, child_valor_g);
+        //     std::cout << "Not found Error\n";
+        //     open.push(*new_board_ptr);
+        // }
+    }
+}
+
+void calc(tipo &inicial)
 {
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    
+    open.push(*(new board(inicial)));
     
     while(!open.empty())
     {
         auto h = open.top();
+        std::cout << "Tirou um nodo de g = " << h.valor_g << "\n";
         
         if(isObjetivo(h)) {
             std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -165,8 +174,9 @@ void calc(board &inicial)
         }
         else{
             open.pop();
-            h.expande(open);
-            closed.insert(h.ordem);  
+            expande(h);
+            std::cout << "Expandiu nodo\n";
+            //closed.insert(h.ordem);
         }
     }
 }
@@ -180,14 +190,13 @@ int main(int argc, char* argv[])
     
     if(argc == 11){
         if (argv[1] == "-astar"sv || argv[1] == "--help"sv) {
-            std::cout << "List of commands:...";
+            std::cout << "List of commands:...\n";
             
-            array<int,9> bd;            
-            for(int i=0; i<9; i++) { 
-                bd.at(i) = atoi(arg[i+2]));
+            tipo bd;            
+            for(char i=0; i<9; i++) { 
+                bd.at(i) = atoi(argv[i+2]);
             }
-            board inst(bd);
-            calc(inst);
+            calc(bd);
         }
     }
     else std::cout << "Sorry, we are not yet ready for that\nTry again later\n\n";
